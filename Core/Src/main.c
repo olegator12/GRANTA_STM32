f4804a3f50da4822946cba6300c39169daf88980
olegator111111 <gipigao081@gmail.com>
2024-03-21 15:48:36 +0300

    Moved files into another folder

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "pedals.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
